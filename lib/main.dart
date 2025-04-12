20ee6813decd345d3de862015b026658e4a0e03b
lamializa991 <lamializa991@gmail.com>
2025-04-12 20:48:31 +0600

    track 4

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final items = List.generate(20, (index) => "Item ${index + 1}");
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("ListView Example")),
-        body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) => ListTile(title: Text(items[index])),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Big Text', style: TextStyle(fontSize: 30)),
+              Text('Bold Text', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Red Text', style: TextStyle(color: Colors.red)),
+            ],
+          ),
         ),
       ),
     );
