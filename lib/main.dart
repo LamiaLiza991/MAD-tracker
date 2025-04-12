9d9fd57db286fbac80e4aeb85e150038e0625600
lamializa991 <lamializa991@gmail.com>
2025-04-12 20:58:40 +0600

    track 6

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: FirstScreen()));
+void main() => runApp(MyApp());
 
-class FirstScreen extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("First Screen")),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Go to Second"),
-          onPressed:
-              () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondScreen()),
-              ),
-        ),
-      ),
-    );
+    return MaterialApp(home: LoginScreen());
   }
 }
 
-class SecondScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String username = '', password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Second Screen")),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Back"),
-          onPressed: () => Navigator.pop(context),
+      appBar: AppBar(title: Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: "Username"),
+                validator:
+                    (value) => value!.isEmpty ? "Please enter username" : null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Password"),
+                obscureText: true,
+                validator:
+                    (value) => value!.isEmpty ? "Please enter password" : null,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Login success
+                  }
+                },
+                child: Text("Submit"),
+              ),
+            ],
+          ),
         ),
       ),
     );
