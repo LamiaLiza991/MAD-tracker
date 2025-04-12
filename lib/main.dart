01f419dae70e71da377e841e6e2b3071b8a85c34
lamializa991 <lamializa991@gmail.com>
2025-04-12 20:35:23 +0600

    track3

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String text = "Hello";
+class MyApp extends StatelessWidget {
+  final items = List.generate(20, (index) => "Item ${index + 1}");
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(text),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    text = "Button Pressed";
-                  });
-                },
-                child: Text("Press Me"),
-              ),
-            ],
-          ),
+        appBar: AppBar(title: Text("ListView Example")),
+        body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) => ListTile(title: Text(items[index])),
         ),
       ),
     );
