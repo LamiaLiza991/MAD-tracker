4659346c82da8207a583104979cb47810cc35812
lamializa991 <lamializa991@gmail.com>
2025-04-12 23:12:18 +0600

    track 19

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,74 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<String> items = List.generate(10, (i) => 'Item ${i + 1}');
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: CustomDrawerApp());
+  }
+}
+
+class CustomDrawerApp extends StatefulWidget {
+  @override
+  _CustomDrawerAppState createState() => _CustomDrawerAppState();
+}
+
+class _CustomDrawerAppState extends State<CustomDrawerApp>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _drawerAnimation;
+  bool _isDrawerOpen = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _drawerAnimation = Tween<Offset>(
+      begin: Offset(-1.0, 0),
+      end: Offset.zero,
+    ).animate(_controller);
+  }
+
+  void _toggleDrawer() {
+    setState(() {
+      _isDrawerOpen = !_isDrawerOpen;
+      _isDrawerOpen ? _controller.forward() : _controller.reverse();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Swipe List")),
-        body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return Dismissible(
-              key: Key(items[index]),
-              background: Container(
-                color: Colors.red,
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.only(left: 20),
-                child: Icon(Icons.delete, color: Colors.white),
-              ),
-              secondaryBackground: Container(
-                color: Colors.blue,
-                alignment: Alignment.centerRight,
-                padding: EdgeInsets.only(right: 20),
-                child: Icon(Icons.edit, color: Colors.white),
-              ),
-              onDismissed: (direction) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('${items[index]} dismissed')),
-                );
-              },
-              child: ListTile(title: Text(items[index])),
-            );
-          },
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Custom Drawer"),
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: _toggleDrawer,
+            ),
+          ),
+          body: Center(child: Text("Main Content")),
         ),
-      ),
+        SlideTransition(
+          position: _drawerAnimation,
+          child: SafeArea(
+            child: Container(
+              width: 250,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  ListTile(title: Text("Option 1")),
+                  ListTile(title: Text("Option 2")),
+                  ListTile(title: Text("Close"), onTap: _toggleDrawer),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
